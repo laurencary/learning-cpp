27c28727d418c1a67dd8f3cc0b354afa9e0bf3f4
laurencary <armstronglg720@gmail.com>
2023-11-16 06:50:04 -0800

    first commit

---
+++ linkedin-learning/Exercise Files/Ch01/01_04/CodeDemo/CodeDemo.cpp
@@ -0,0 +1,19 @@
+// Learning C++ 
+// Challenge 01_04
+// Console Interaction, by Eduardo Corpeño 
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+	cout << "Please type your name below:" << endl;
+
+	string str;
+	cin >> str;
+
+
+	cout << "Hi, " << str << endl;
+	return(0);
+}
